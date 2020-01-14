4d6655c5f5c307e261791d136c9c02cdecee055c
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2020-01-14 09:21:57 +0700

    add custom post

--- app/public/wp-content/themes/wp-gatsby-js-theme-starter-master/functions.php
+++ app/public/wp-content/themes/wp-gatsby-js-theme-starter-master/functions.php
@@ -1,3 +1,23 @@
 <?php
-add_theme_support( 'custom-logo' );
-add_theme_support( 'menus' );
+add_theme_support('custom-logo');
+add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+function create_custom_portfolio_post_type()
+{
+  register_post_type(
+    'portofolio',
+    array(
+      'labels' => array(
+        'name' => __('Portofolio'),
+        'singular_name' => __('Portofolio')
+      ),
+      'public' => true,
+      'show_in_admin_bar' => true,
+      'show_in_rest' => true
+    )
+  );
+  add_post_type_support('portofolio', array('thumbnail', 'excerpt'));
+}
+
+add_action('init', 'create_custom_portfolio_post_type');
